d1b1c08e6c7a5aa55387d8155e2df989da82c0a9
Geordano Lima <geordanofl@gmail.com>
2024-09-11 20:07:45 -0300

    adicionado testes do processo de cadastro da semana 4 (sem banco de dados)

--- testes_de_aplicao/semana_4/cadastro.py
+++ testes_de_aplicao/semana_4/cadastro.py
@@ -33,26 +33,37 @@ class Cadastro:
         _cpf = self._valida_cpf(pessoa.cpf)
         if _cpf:
             return _cpf
+        _senha = self._valida_senha(senha=pessoa.senha)
+        if _senha:
+            return _senha
         if pessoa.senha != confirma_senha:
             return "As senhas não coincidem!"
     
     def _valida_nome(self, nome: str):
         sobrenomes_um_caracter = ['e']
         nomes = nome.strip().split(' ')
+        if len(nome) >= 100:
+            return "o nome precisa ter até 100 caracters"
         if len(nomes) <= 1:
             return "preencha o sobrenome também"
         for nome in nomes:
             if len(nome) < 2 and nome not in sobrenomes_um_caracter:
-                return "verifique o nome digitado"
+                return "o nome precisa ter ao menos 2 caracteres"
             if nome.count(nome[0]) == len(nome) and nome not in sobrenomes_um_caracter:
                 return "não é permitido nomes com todos os caracteres iguais"
         return None
 
     def _valida_email(self, email: str):
+        if len(email) >= 50:
+            return "o email pode ter somente 50 caracteres"
         if email.find('@') < 3:
             return "email precisa ter @"
         if email.find('.') < email.find('@'):
             return "email precisa ter . após o @"
+        if email[-1] == '.':
+            return "email não pode terminar em ."
+        if email[email.find("@")+1] == ".":
+            return "não pode ter . logo após o @"
         return None
     
     def _valida_cpf(self, cpf: str):
@@ -64,7 +75,25 @@ class Cadastro:
             return None
         except Exception:
             return "CPF inválido"
-    
+
+    def _valida_senha(self, senha: str):
+        especiais = ["!","@","#","$","%","&","*","(",")","-","_","+","=",".",",","\\","|","'",'"']
+        count = 0
+        for char in especiais:
+            if senha.find(char) >= 0:
+                count = 1
+        if not count:
+            return "é necessário possuir caracter especial"
+        count = 0
+        for num in range(0, 10):
+            if senha.find(str(num)) >= 0:
+                count = 1
+        if not count:
+            return "é necessário possuir valores numericos"
+        if len(senha) < 8 or len(senha) > 16:
+            return "a senha deve ter entre 8 e 16 caracteres"
+        return None
+        
     def _salvar(self, obj: dict):
         arquivo = open("dados.json", "a")
         json.dump(obj, arquivo)
---
+++ testes_de_aplicao/semana_4/test_cadastro.py
@@ -0,0 +1,134 @@
+import pytest
+from .cadastro import Pessoa, Cadastro
+
+
+@pytest.fixture
+def cadastro() -> Cadastro:
+    return Cadastro()
+
+
+# testes de unidade
+@pytest.mark.parametrize(
+    "nome,resultado_esperado", [
+        ("joão da silva", None),
+        ("Maria de Lurdes e Pereira", None),
+        ("  nome com espaço antes e depois  ", None),
+        (
+            "nome com mais de 100 caracteres nome com mais de 100 caracteres nome com mais de 100 caracteres nome com",
+            "o nome precisa ter até 100 caracters"
+        ),
+        ("nomesemespaco", "preencha o sobrenome também"),
+        ("joao a silva", "o nome precisa ter ao menos 2 caracteres"),
+        ("joão da aaaaaaaaa", "não é permitido nomes com todos os caracteres iguais")
+    ]
+)
+def test_nome(cadastro, nome, resultado_esperado):
+    assert cadastro._valida_nome(nome=nome) == resultado_esperado
+
+
+@pytest.mark.parametrize(
+    "email,resultado_esperado", 
+    [
+        ("email@email.com", None),
+        ("email@teste.edu.br", None),
+        ("emailcommaisde50caracteresemailcommaisde50caracteres", "o email pode ter somente 50 caracteres"),
+        ("emailsemarroba.com.br", "email precisa ter @"),
+        ("emailcom@massemponto", "email precisa ter . após o @"),
+        ("emailcom@ecom.terminadoem.", "email não pode terminar em ."),
+        ("email@.com", "não pode ter . logo após o @")
+    ]
+)
+def test_email(cadastro, email, resultado_esperado):
+    assert cadastro._valida_email(email=email) == resultado_esperado
+
+
+@pytest.mark.parametrize(
+    "cpf,resultado_esperado", 
+    [
+        ("997.251.290-83", None),
+        ("76739421005", None),
+        ("111111111111111", "CPF inválido"),
+        ("000111", "CPF inválido"),
+        ("teste", "CPF inválido"),
+        ("0001++11", "CPF inválido"),
+        ("00011__221", "CPF inválido"),
+    ]
+)
+def test_cpf(cadastro, cpf, resultado_esperado):
+    assert cadastro._valida_cpf(cpf=cpf) == resultado_esperado
+
+
+@pytest.mark.parametrize(
+    "senha,resultado_esperado",
+    [
+        ("Teste@123456", None),
+        ("1testedeerro", "é necessário possuir caracter especial"),
+        ("@@@@@@@@@@@@", "é necessário possuir valores numericos"),
+        ("c1rt@", "a senha deve ter entre 8 e 16 caracteres"),
+        ("esta_senh@_é_bem_long4", "a senha deve ter entre 8 e 16 caracteres")
+    ]
+)
+def test_senha(cadastro, senha, resultado_esperado):
+    assert cadastro._valida_senha(senha=senha) == resultado_esperado
+
+
+# testes de componente
+@pytest.mark.parametrize(
+    "pessoa,conf_senha,resultado_esperado",
+    [
+        (Pessoa(nome="nome teste", email="email@email.com", cpf="76739421005", senha="Teste@1234"), "Teste@1234", None),
+        (Pessoa(nome="", email="email", cpf="123", senha="senha"), "", "Preencha todos os campos"),
+        (Pessoa(nome="nome", email="", cpf="123", senha="senha"), "", "Preencha todos os campos"),
+        (Pessoa(nome="nome", email="email", cpf="", senha="senha"), "", "Preencha todos os campos"),
+        (Pessoa(nome="nome", email="email", cpf="123", senha=""), "", "Preencha todos os campos"),
+        (Pessoa(nome="nome", email="email", cpf="123", senha="senha"), "", "preencha o sobrenome também"),
+        (Pessoa(nome="nome teste", email="email", cpf="123", senha="senha"), "", "email precisa ter @"),
+        (Pessoa(nome="nome teste", email="email@email.com", cpf="123", senha="senha"), "", "CPF inválido"),
+        (
+            Pessoa(nome="nome teste", email="email@email.com", cpf="76739421005", senha="Teste"),
+            "",
+            "é necessário possuir caracter especial"
+        ),
+        (
+            Pessoa(nome="nome teste", email="email@email.com", cpf="76739421005", senha="Teste@1234"),
+            "",
+            "As senhas não coincidem!"
+        ),
+        
+    ]
+)
+def test_validacao(cadastro, pessoa, conf_senha, resultado_esperado):
+    assert cadastro.validacao(pessoa=pessoa, confirma_senha=conf_senha) == resultado_esperado
+
+
+@pytest.mark.parametrize(
+    "pessoa,conf_senha,resultado_esperado",
+    [
+        (
+            Pessoa(nome="nome teste", email="email@email.com", cpf="76739421005", senha="Teste@1234"),
+            "Teste@1234",
+            (True, {"nome":"nome teste", "email":"email@email.com", "cpf":"76739421005", "senha":"Teste@1234"})
+        ),
+        (Pessoa(nome="", email="email", cpf="123", senha="senha"), "", (False, "Preencha todos os campos")),
+        (Pessoa(nome="nome", email="", cpf="123", senha="senha"), "", (False, "Preencha todos os campos")),
+        (Pessoa(nome="nome", email="email", cpf="", senha="senha"), "", (False, "Preencha todos os campos")),
+        (Pessoa(nome="nome", email="email", cpf="123", senha=""), "", (False, "Preencha todos os campos")),
+        (Pessoa(nome="nome", email="email", cpf="123", senha="senha"), "", (False, "preencha o sobrenome também")),
+        (Pessoa(nome="nome teste", email="email", cpf="123", senha="senha"), "", (False, "email precisa ter @")),
+        (Pessoa(nome="nome teste", email="email@email.com", cpf="123", senha="senha"), "", (False, "CPF inválido")),
+        (
+            Pessoa(nome="nome teste", email="email@email.com", cpf="76739421005", senha="Teste"),
+            "",
+            (False, "é necessário possuir caracter especial")
+        ),
+        (
+            Pessoa(nome="nome teste", email="email@email.com", cpf="76739421005", senha="Teste@1234"),
+            "",
+            (False, "As senhas não coincidem!")
+        ),
+    ]
+)
+def test_cadastro(mocker, cadastro, pessoa, conf_senha, resultado_esperado):
+    # para sobrescrever o metodo salvar, afim de que ele não salve as informações no arquivo
+    mocker.patch.object(Cadastro, "_salvar", return_value=None)
+    assert cadastro.cadastrar(pessoa=pessoa, confirma_senha=conf_senha) == resultado_esperado
